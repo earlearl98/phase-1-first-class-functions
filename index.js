4eba7ece674ca91b772b11edd0ded873b7fd6e41
earlearl98 <earlearl1720@gmail.com>
2023-09-20 16:48:15 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+function receivesAFunction(callback) {
+  // Your code here
+  callback();
+}
+
+
+function returnsANamedFunction() {
+  // Your code here
+  return function namedFunction() {
+    // function body 
+  }
+
+
+}
+
+
+function returnsAnAnonymousFunction() {
+  // Your code here
+  return function() {
+    // function body
+  };
+}
+
+
